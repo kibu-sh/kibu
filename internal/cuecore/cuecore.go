b96b4c5e1a206f10d71ee9a5da38b03dbf6df24d
jqualls <qualls.james@gmail.com>
2022-12-01 07:57:48 -0700

    chore: updated cuecore test to decode nested go structs

--- internal/cuecore/cuecore.go
+++ internal/cuecore/cuecore.go
@@ -1,41 +1,101 @@
 package cuecore
 
 import (
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/build"
 	"cuelang.org/go/cue/cuecontext"
 	"cuelang.org/go/cue/load"
-	"github.com/discernhq/devx/internal/codedef"
+	"errors"
 )
 
-type LoadOptions struct {
+type Loader struct {
 	Dir        string
 	Entrypoint []string
+
+	Ctx       *cue.Context
+	Instances []*build.Instance
+	Values    []cue.Value
 }
 
-func Load(opts LoadOptions) (mod codedef.Module, err error) {
-	ctx := cuecontext.New()
-	instances := load.Instances(opts.Entrypoint, &load.Config{
-		Dir: opts.Dir,
-		//Package: "_",
-		//DataFiles:   false,
-		//Overlay:     nil,
-		//Stdin:       nil,
-	})
+type LoadOption func(l *Loader) error
 
-	values, err := ctx.BuildInstances(instances)
-	if err != nil {
+func WithDir(dir string) LoadOption {
+	return func(l *Loader) error {
+		l.Dir = dir
+		return nil
+	}
+}
+
+func WithEntrypoint(entrypoint []string) LoadOption {
+	return func(l *Loader) error {
+		l.Entrypoint = entrypoint
+		return nil
+	}
+}
+
+func WithCueContext(ctx *cue.Context) LoadOption {
+	return func(l *Loader) error {
+		l.Ctx = ctx
+		return nil
+	}
+}
+
+func WithValidation() LoadOption {
+	return func(l *Loader) (err error) {
+		for _, value := range l.Values {
+			err = value.Validate()
+			if err != nil {
+				return
+			}
+		}
 		return
 	}
+}
 
-	err = values[0].Validate()
-	if err != nil {
+func WithBasicDecoder[T any](v *T) LoadOption {
+	return func(l *Loader) error {
+		if len(l.Values) == 0 {
+			return errors.New("no cue values to decode into")
+		}
+		return l.Values[0].Decode(v)
+	}
+}
+
+func WithBuild() LoadOption {
+	return func(l *Loader) (err error) {
+		l.Instances = load.Instances(l.Entrypoint, &load.Config{
+			Dir: l.Dir,
+			// Package: "_",
+			// DataFiles:   false,
+			// Overlay:     nil,
+			// Stdin:       nil,
+		})
+
+		l.Values, err = l.Ctx.BuildInstances(l.Instances)
+		if err != nil {
+			return
+		}
 		return
 	}
+}
 
-	mod.Name = instances[0].PkgName
-
-	err = values[0].Decode(&mod)
-	if err != nil {
-		return
+func Load(opts ...LoadOption) (l *Loader, err error) {
+	l = new(Loader)
+	for _, opt := range opts {
+		if err = opt(l); err != nil {
+			return
+		}
 	}
 	return
+}
+
+func LoadWithDefaults(dir string, entrypoint []string, opts ...LoadOption) (l *Loader, err error) {
+	opts = append([]LoadOption{
+		WithDir(dir),
+		WithEntrypoint(entrypoint),
+		WithCueContext(cuecontext.New()),
+		WithBuild(),
+	}, opts...)
+
+	return Load(opts...)
 }
--- internal/cuecore/cuecore_test.go
+++ internal/cuecore/cuecore_test.go
@@ -1,21 +1,33 @@
 package cuecore
 
 import (
+	"github.com/rogpeppe/go-internal/testscript"
 	"github.com/stretchr/testify/require"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
 func TestLoad(t *testing.T) {
-	cwd, err := os.Getwd()
-	require.NoError(t, err)
-	testdata := filepath.Join(cwd, "../testdata")
-	workflowFile := filepath.Join(testdata, "module.cue")
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata",
+		Setup: func(env *testscript.Env) error {
+			return nil
+		},
+		Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
+			"load": func(ts *testscript.TestScript, neg bool, args []string) {
+				cwd := ts.Getenv("WORK")
+				var mod struct {
+					Nested struct {
+						Name string
+					}
+				}
 
-	_, err = Load(LoadOptions{
-		Dir:        cwd,
-		Entrypoint: []string{workflowFile},
+				_, err := LoadWithDefaults(cwd, []string{"module.cue"},
+					WithValidation(),
+					WithBasicDecoder(&mod),
+				)
+				ts.Check(err)
+				require.Equal(t, "example", mod.Nested.Name)
+			},
+		},
 	})
-	require.NoError(t, err)
 }
