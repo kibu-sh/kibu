546f71f4ab777b0061d08615aa8740575ddc51ce
jqualls <qualls.james@gmail.com>
2022-12-07 19:33:28 -0700

    feat(ctxutil): provides a generic container for passing values through context.Context

---
+++ pkg/ctxutil/ctxutil.go
@@ -0,0 +1,34 @@
+package ctxutil
+
+import (
+	"context"
+	"github.com/pkg/errors"
+)
+
+var ErrNotFoundInContext = errors.New("not found in context")
+
+type Store[T any] struct {
+	key any
+}
+
+func (s *Store[T]) Save(ctx context.Context, v *T) context.Context {
+	return context.WithValue(ctx, s.key, v)
+}
+
+func (s *Store[T]) Load(ctx context.Context) (*T, error) {
+	v := ctx.Value(s.key)
+	if v == nil {
+		return nil, errors.Wrapf(
+			ErrNotFoundInContext,
+			"by key %T", s.key,
+		)
+	}
+
+	return v.(*T), nil
+}
+
+func NewStore[T any](key any) *Store[T] {
+	return &Store[T]{
+		key: key,
+	}
+}
---
+++ pkg/ctxutil/ctxutil_test.go
@@ -0,0 +1,29 @@
+package ctxutil
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNewContextLoader(t *testing.T) {
+	key := struct{}{}
+	type User struct {
+		ID string
+	}
+
+	store := NewStore[User](key)
+
+	t.Run("should be able to save and load a value from a context", func(t *testing.T) {
+		expected := &User{ID: "test"}
+		ctx := store.Save(context.Background(), expected)
+		actual, err := store.Load(ctx)
+		require.NoError(t, err)
+		require.Equal(t, expected, actual)
+	})
+
+	t.Run("should return an error when the value is not found in the context", func(t *testing.T) {
+		_, err := store.Load(context.Background())
+		require.ErrorIs(t, err, ErrNotFoundInContext)
+	})
+}
