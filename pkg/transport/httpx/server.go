fd14f6c6ba4fbb8c8c7278a658f977e21775e351
jqualls <qualls.james@gmail.com>
2022-12-07 20:52:46 -0700

    feat: implements a smarter server start

--- pkg/transport/httpx/server.go
+++ pkg/transport/httpx/server.go
@@ -2,52 +2,76 @@ package httpx
 
 import (
 	"context"
-	"github.com/discernhq/devx/pkg/transport"
+	"github.com/samber/lo"
 	"net"
 	"net/http"
+	"time"
 )
 
 type Server struct {
 	*http.Server
-	l net.Listener
+	Listener        net.Listener
+	shutdownTimeout *time.Duration
+	shutdownChan    chan error
 }
 
 func (s *Server) Start(ctx context.Context) error {
-	return s.Serve(s.l)
+	startChan := make(chan error)
+
+	go func() {
+		close(startChan)
+		_ = s.Serve(s.Listener)
+	}()
+
+	go func() {
+		<-ctx.Done()
+		sCtx, cancel := context.WithTimeout(context.Background(), *s.shutdownTimeout)
+		defer cancel()
+		defer close(s.shutdownChan)
+		s.shutdownChan <- s.Shutdown(sCtx)
+	}()
+
+	return <-startChan
 }
 
 func (s *Server) Stop(ctx context.Context) error {
 	return s.Shutdown(ctx)
 }
 
-func OnStart(ctx context.Context, s transport.Server) error {
-	// TODO: this might not be super great
-	// lets think about what happens when this errors
-	go s.Start(ctx)
-	return nil
-}
-
-func OnStop(ctx context.Context, s transport.Server) error {
-	return s.Stop(ctx)
+func (s *Server) Wait() error {
+	// TODO: think about our timeouts
+	return <-s.shutdownChan
 }
 
 type NewServerParams struct {
-	Addr string
-	Mux  ServeMux
+	Addr     ListenAddr
+	Mux      ServeMux
+	Handlers []*Handler
 }
 
-func NewServer(params *NewServerParams) (transport.Server, error) {
-	s := &http.Server{
-		Addr:    params.Addr,
-		Handler: params.Mux,
-	}
+type ListenAddr string
 
-	l, err := net.Listen("tcp", s.Addr)
+func NewServer(params *NewServerParams) (*Server, error) {
+	listener, err := net.Listen("tcp", string(params.Addr))
 	if err != nil {
 		return nil, err
 	}
 
+	// TODO: register global middleware
+	// register handlers with mux router
+	for _, handler := range params.Handlers {
+		params.Mux.Handle(handler)
+	}
+
 	return &Server{
-		s, l,
+		Server: &http.Server{
+			Addr:    string(params.Addr),
+			Handler: params.Mux,
+		},
+		Listener: listener,
+
+		// buffered to avoid blocking on shutdown
+		shutdownChan:    make(chan error, 1),
+		shutdownTimeout: lo.ToPtr(time.Second * 30),
 	}, nil
 }
