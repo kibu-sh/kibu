44b17325d0f410b88fb20b4a17cc412c3c1f2884
jqualls <qualls.james@gmail.com>
2023-01-26 06:48:50 -0700

    feat: add google secret manager store

---
+++ pkg/config/gcp_secret_manager_store_test.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestGCPSecretManagerStore(t *testing.T) {
+	ctx := context.Background()
+
+	temp, err := os.MkdirTemp("", "cloud_store_test")
+	require.NoError(t, err)
+	defer os.RemoveAll(temp)
+
+	s, err := NewGCPSecretManagerStore(ctx, "discern-devops")
+	require.NoError(t, err)
+	relativeSecretPath := "level1/secret.json"
+
+	var expected = map[string]any{
+		"test": "test",
+	}
+	_, err = s.Set(ctx, SetParams{
+		Path: relativeSecretPath,
+		Data: expected,
+	})
+	require.NoError(t, err)
+
+	var result map[string]any
+	_, err = s.Get(ctx, GetParams{
+		Path:   relativeSecretPath,
+		Result: &result,
+	})
+	require.NoError(t, err)
+	require.Equal(t, expected, result)
+}
