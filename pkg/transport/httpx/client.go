f0653efdb8fee2cc989c12804d1d0fb21bef534a
James R Qualls <sourcec0de@users.noreply.github.com>
2023-02-10 10:17:36 -0700

    feat: Added a new httpx util function to create new JSON http request. (#2)
    Co-authored-by: shals1031 <88051291+shals1031@users.noreply.github.com>

---
+++ pkg/transport/httpx/client.go
@@ -0,0 +1,17 @@
+package httpx
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+func NewJSONRequest[T any](httpMethod string, url string, object *T) (req *http.Request, err error) {
+	payload := new(bytes.Buffer)
+	err = json.NewEncoder(payload).Encode(object)
+	if err != nil {
+		return
+	}
+	//http.MethodGet
+	return http.NewRequest(httpMethod, url, payload)
+}
