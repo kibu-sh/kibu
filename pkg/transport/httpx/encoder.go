8b947d8af70e948cc413cded7aa5094940a4f440
jqualls <qualls.james@gmail.com>
2023-08-02 09:55:58 -0400

    fix: handle unwrapping error messages in JSONErrorDecoder

--- pkg/transport/httpx/encoder.go
+++ pkg/transport/httpx/encoder.go
@@ -3,6 +3,8 @@ package httpx
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/discernhq/devx/pkg/transport"
 	"net/http"
 )
@@ -20,6 +22,10 @@ type DefaultJSONError struct {
 	Status  int    `json:"status"`
 }
 
+func (d DefaultJSONError) Error() string {
+	return fmt.Sprintf("%d: %s", d.Status, d.Message)
+}
+
 func (d DefaultJSONError) GetStatusCode() int {
 	return d.Status
 }
@@ -31,13 +37,11 @@ func (d DefaultJSONError) PrepareResponse() any {
 // JSONErrorEncoder encodes any response as JSON and writes it to the ResponseWriter
 func JSONErrorEncoder() transport.ErrorEncoderFunc {
 	return func(ctx context.Context, writer transport.Response, err error) error {
-		res, ok := err.(transport.ErrorResponse)
-		if !ok {
-			res = DefaultJSONError{
-				Status:  http.StatusInternalServerError,
-				Message: err.Error(),
-			}
+		var res transport.ErrorResponse = DefaultJSONError{
+			Status:  http.StatusInternalServerError,
+			Message: err.Error(),
 		}
+		_ = errors.As(err, &res)
 
 		writer.SetStatusCode(res.GetStatusCode())
 		writer.Headers().Set("Content-Type", "application/json")
---
+++ pkg/transport/httpx/encoder_test.go
@@ -0,0 +1,87 @@
+package httpx
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/discernhq/devx/pkg/transport"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+)
+
+type mockTransportResponse struct {
+	mock.Mock
+	headers http.Header
+	buf     *bytes.Buffer
+}
+
+func (m *mockTransportResponse) Write(p []byte) (n int, err error) {
+	return m.buf.Write(p)
+}
+
+func (m *mockTransportResponse) Headers() http.Header {
+	m.Called()
+	return m.headers
+}
+
+func (m *mockTransportResponse) SetStatusCode(i int) {
+	m.Called(i)
+}
+
+func (m *mockTransportResponse) DelCookie(cookie http.Cookie) transport.Response {
+	panic("should never be called")
+}
+
+func (m *mockTransportResponse) DelCookieByName(name string) transport.Response {
+	panic("should never be called")
+}
+
+func (m *mockTransportResponse) SetCookie(cookie http.Cookie) transport.Response {
+	panic("should never be called")
+}
+
+func (m *mockTransportResponse) Redirect(req transport.Request, url string, code int) {
+	panic("should never be called")
+}
+
+func (m *mockTransportResponse) Underlying() any {
+	panic("should never be called")
+}
+
+func TestJSONErrorEncoder(t *testing.T) {
+	ctx := context.Background()
+	encoder := JSONErrorEncoder()
+	t.Run("should properly encode wrapped transport error", func(t *testing.T) {
+		resp := &mockTransportResponse{
+			headers: http.Header{},
+			buf:     new(bytes.Buffer),
+		}
+		expected := DefaultJSONError{
+			Message: "Testing",
+			Status:  http.StatusNotFound,
+		}
+		expectedBytes, err := json.Marshal(expected)
+		require.NoError(t, err)
+		resp.On("SetStatusCode", http.StatusNotFound).Return()
+		resp.On("Headers").Return(http.Header{})
+		err = encoder(ctx, resp, errors.Join(errors.New("testing"), expected))
+		require.NoError(t, err)
+		require.JSONEq(t, string(expectedBytes), resp.buf.String())
+	})
+
+	t.Run("should properly use default error message", func(t *testing.T) {
+		resp := &mockTransportResponse{
+			headers: http.Header{},
+			buf:     new(bytes.Buffer),
+		}
+		resp.On("SetStatusCode", http.StatusInternalServerError).Return()
+		resp.On("Headers").Return(http.Header{})
+		err := encoder(ctx, resp, errors.New("broken"))
+		require.NoError(t, err)
+		require.Contains(t, resp.buf.String(), "broken")
+		require.Equal(t, resp.Headers().Get("Content-Type"), "application/json")
+	})
+}
