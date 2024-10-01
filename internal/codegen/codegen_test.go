3c10b6d2f1dd660be43f394d070b60718aa78dc4
James R Qualls <Qualls.James@gmail.com>
2024-10-01 14:40:18 -0700

    fix: remove unneeccary render loop

--- internal/codegen/codegen_test.go
+++ internal/codegen/codegen_test.go
@@ -10,22 +10,20 @@ import (
 
 func TestGenerate(t *testing.T) {
 	cwd, _ := os.Getwd()
-	// Run the testscript 10 times to ensure that the test is deterministic.
-	for i := 0; i < 10; i++ {
-		testscript.Run(t, testscript.Params{
-			Dir: filepath.Join(cwd, "testdata"),
-			Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
-				"parse": func(ts *testscript.TestScript, neg bool, args []string) {
-					ts.Check(Generate(GenerateParams{
-						Patterns:  []string{"./..."},
-						Pipeline:  DefaultPipeline(),
-						Dir:       filepath.Join(args[0], "src"),
-						OutputDir: filepath.Join(args[0], "gen"),
-					}))
-				},
+	testscript.Run(t, testscript.Params{
+		Dir: filepath.Join(cwd, "testdata"),
+		//UpdateScripts: true,
+		Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
+			"parse": func(ts *testscript.TestScript, neg bool, args []string) {
+				ts.Check(Generate(GenerateParams{
+					Patterns:  []string{"./..."},
+					Pipeline:  DefaultPipeline(),
+					Dir:       filepath.Join(args[0], "src"),
+					OutputDir: filepath.Join(args[0], "gen"),
+				}))
 			},
-		})
-	}
+		},
+	})
 }
 
 func TestPackageScopedID(t *testing.T) {
