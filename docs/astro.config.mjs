806ef0e6b678dfe3a10fb9027db54b517354623e
James R Qualls <sourcec0de@users.noreply.github.com>
2024-10-01 19:34:16 -0700

    fix: point docs at primary domain (#5)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kibu.sh',
   integrations: [
     starlight({
       title: 'Kibu Documentation',
