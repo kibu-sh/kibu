1fbaa718c7090fd6c910f6d071cae60d57c9079a
James Qualls <qualls.james@gmail.com>
2026-09-03 21:34:26 -0700

    chore(nix): add direnv flake with Go 1.27

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Development environment for kibu";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.go_1_27
+            ];
+
+            env = {
+              # Use the flake's Go instead of auto-downloading another toolchain.
+              GOTOOLCHAIN = "local";
+            };
+          };
+        }
+      );
+    };
+}
